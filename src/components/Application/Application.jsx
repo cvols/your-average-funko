07d57125bd67a53d9d54ec8b99ea15b73c52c46d
Matt Volonnino <mvolonnino12@gmail.com>
2020-12-01 17:32:02 -0500

    added custom brand image

--- src/components/Application/Application.jsx
+++ src/components/Application/Application.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useDataLayerValue } from "../../context/DataLayer";
 import { auth } from "../../fire";
+import funkoBrand from "../../img/funkoBrand.png";
+
+import "./Application.css";
 
 function Application() {
   const [{ user }, dispatch] = useDataLayerValue();
@@ -8,6 +11,7 @@ function Application() {
   return (
     <div className="application container">
       <div className="jumbotron">
+        <img src={funkoBrand} alt="" />
         <h1>{`Logged in as ${user?.displayName}`}</h1>
         <button
           className="btn btn-danger"
