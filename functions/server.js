cbd9d4a6f6b5d9775d3402d4158739e713cd7cbd
cvols <chris.volonnino@gmail.com>
2020-12-02 16:38:22 -0500

    adding puppeter

--- functions/server.js
+++ functions/server.js
@@ -3,6 +3,7 @@ const serviceAccount = require('./permissions.json');
 const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
+const puppeteer = require('puppeteer');
 
 const app = express();
 app.use(cors({ origin: true }));
@@ -16,6 +17,60 @@ const db = admin.firestore();
 app.get('/hello-world', (req, res) => {
   return res.status(200).send('Hello World!');
 });
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null
+  });
+  const page = await browser.newPage();
+  const url = 'https://www.cardboardconnection.com/brand/funko/funko-pop';
+  await page.goto(url, {
+    waitUntil: 'domcontentloaded'
+  });
+  await page.waitForSelector('.product-subtitle');
+
+  // const results = await page.evaluate(() => {
+  //   const urlSelector = document.querySelectorAll('.product-subtitle a');
+  //   const urls = [...urlSelector];
+  //   return urls.map(url => url.innerHTML);
+  // });
+
+  // results.map(async result => {
+  //   console.log('result: ', result);
+  // });
+
+  await page.$$eval('.product-subtitle a', links => {
+    return links.forEach(async link => {
+      link.click();
+
+      const [el] = await page.$x(
+        '//*[@id="post-main-121693"]/div[1]/div[1]/h1'
+      );
+      const txt = await el.getProperty('textContent');
+      const rawText = await txt.jsonValue();
+      console.log('rawText: ', rawText);
+    });
+  });
+
+  // await page.evaluate(() => {
+  //   const elements = document.querySelectorAll('.product-subtitle a');
+  //   for (let element of elements) {
+  //     element.click();
+  //   }
+  // });
+
+  // const [el] = await page.$x('//*[@id="productListing"]/div[1]/div[2]/h2/a');
+  // const href = await el.getProperty('href');
+
+  // console.log('href: ', href);
+
+  // click works
+  // const href = await page.$('.product-subtitle a');
+  // await href.evaluate(ref => ref.click());
+
+  // await browser.close();
+})();
 
 // create
 app.post('/api/create', (req, res) => {
